189a85fdf55342f685fb5505673dc10c9b27f175
AlexanderSChen <kikikatme@BLD.localdomain>
2022-08-11 15:30:23 -0700

    commit 8 11 22

--- forms.py
+++ forms.py
@@ -23,3 +23,13 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+class UserEditForm(FlaskForm):
+    """Form for current user to edit information"""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    image_url = StringField('Profile Picture')
+    header_image_url = StringField('Header Image')
+    bio = StringField('Bio')
+    password = PasswordField('Password', validators=[Length(min=6)])
